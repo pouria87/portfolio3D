613391764134b3cd7690c0dad11bb3c2774c8607
pouria Issapour <pouria.issapour@gmail.com>
2024-10-16 12:24:28 +1000

    last commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
         </div>
         <div className='col-span-1 xl:row-span-4'>
           <div className='grid-container'>
-            <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center items-center'>
+            <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
               <Globe
                 height={326}
                 width={326}
